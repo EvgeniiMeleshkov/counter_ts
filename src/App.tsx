dced82f2a1e7c8dbb571e0f8890264c842bbbdff
Evgenii <emeleshkov@mail.ru>
2022-08-07 01:17:11 +0300

    Refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,11 @@
 import React from 'react';
 import './App.css';
 import {MyCounter} from './MyCounter/MyCounter';
-import {useDispatch, useSelector} from 'react-redux';
-import {AppRootStateType} from './Redux/store';
-import {setCurrentAC, setMaxAC, setMinAC} from './Redux/SettingReducer';
 
 function App() {
-
-
-    const dispatch = useDispatch()
-    const minValue = useSelector<AppRootStateType, number>(state => state.settings.min)
-    const maxValue = useSelector<AppRootStateType, number>(state => state.settings.max)
-    let value = useSelector<AppRootStateType, number>(state => state.settings.currentValue)
-
-    let disableReset = true
-    let disableIncr = false
-    let error = ''
-
-
-    if(minValue >= maxValue || value % 1 !== 0 || value < 0){
-        disableIncr = true
-        disableReset = false
-        error = 'Enter valid range'
-    }else{
-        error = ''
-        disableReset = false
-    }
-    if(value === maxValue){
-        disableIncr = true
-    }
-    if(value === minValue){
-        disableReset = true
-    }
-    if(error){
-        disableReset = false
-    }
-
-    const settingMINimalValueCallBack = (val: number) => {
-            dispatch(setMinAC(val))
-            dispatch(setCurrentAC(val))
-        if(val > maxValue || val < 0) {
-            dispatch(setMinAC(minValue))
-        }
-    }
-    const settingMAXimalValueCallBack = (val: number) => {
-        dispatch(setMaxAC(val))
-        if(val < minValue) {
-            dispatch(setMaxAC(maxValue))
-        }
-    }
-    const onIncrementClick = () => {
-        dispatch(setCurrentAC(value += 1))
-    }
-    const onReset = () => {
-        dispatch(setMinAC(minValue))
-        dispatch(setMaxAC(maxValue))
-        dispatch(setCurrentAC(minValue))
-    }
-
-    return (
+        return (
         <div className="App">
-            <MyCounter activateReset={onReset}
-                       callBackIncr={onIncrementClick}
-                       value={value}
-                       disableIncr={disableIncr}
-                       disableReset={disableReset}
-                       settingMAXimalValueCallBack={settingMAXimalValueCallBack}
-                       settingMINimalValueCallBack={settingMINimalValueCallBack}
-                       maxValue={maxValue}
-                       minValue={minValue}
-                       error={error}/>
+            <MyCounter/>
         </div>
     );
 }
--- src/MyCounter/MyCounter.tsx
+++ src/MyCounter/MyCounter.tsx
@@ -6,34 +6,69 @@ import {Input} from '../components/Input/Input';
 import {useDispatch, useSelector} from 'react-redux';
 import {AppRootStateType} from '../Redux/store';
 import {editAC} from '../Redux/editReducer';
+import {setCurrentAC, setMaxAC, setMinAC} from '../Redux/SettingReducer';
+//
+// type MyCounterPropsType = {
+//     disableIncr: boolean
+//     disableReset: boolean
+//     settingMAXimalValueCallBack: (val: number) => void
+//     settingMINimalValueCallBack: (val: number) => void
+//     error: string
+// }
+export const MyCounter: React.FC = () => {
 
-type MyCounterPropsType = {
-    activateReset: () => void
-    callBackIncr: () => void
-    value: number
-    disableIncr: boolean
-    disableReset: boolean
-    settingMAXimalValueCallBack: (val: number) => void
-    settingMINimalValueCallBack: (val: number) => void
-    maxValue: number
-    minValue: number
-    error: string
-}
-export const MyCounter: React.FC<MyCounterPropsType> = ({
-                                                            activateReset,
-                                                            disableReset,
-                                                            disableIncr,
-                                                            settingMINimalValueCallBack,
-                                                            settingMAXimalValueCallBack,
-                                                            maxValue,
-                                                            minValue,
-                                                            value,
-                                                            callBackIncr,
-                                                            error
-                                                        }) => {
 
+    const minValue = useSelector<AppRootStateType, number>(state => state.settings.min)
+    const maxValue = useSelector<AppRootStateType, number>(state => state.settings.max)
+    let value = useSelector<AppRootStateType, number>(state => state.settings.currentValue)
     const edit = useSelector<AppRootStateType, boolean>(state => state.edition.edit)
     const dispatch = useDispatch()
+
+    let disableReset
+    let disableIncr
+    let error
+
+
+    if(minValue >= maxValue || value % 1 !== 0 || value < 0){
+        disableIncr = true
+        disableReset = false
+        error = 'Enter valid range'
+    }else{
+        error = ''
+        disableReset = false
+    }
+    if(value === maxValue){
+        disableIncr = true
+    }
+    if(value === minValue){
+        disableReset = true
+    }
+    if(error){
+        disableReset = false
+    }
+
+    const settingMINimalValueCallBack = (val: number) => {
+        dispatch(setMinAC(val))
+        dispatch(setCurrentAC(val))
+        if(val > maxValue || val < 0) {
+            dispatch(setMinAC(minValue))
+        }
+    }
+    const settingMAXimalValueCallBack = (val: number) => {
+        dispatch(setMaxAC(val))
+        if(val < minValue) {
+            dispatch(setMaxAC(maxValue))
+        }
+    }
+
+    const callBackIncr = () => {
+        dispatch(setCurrentAC(value += 1))
+    }
+    const activateReset = () => {
+        dispatch(setMinAC(minValue))
+        dispatch(setMaxAC(maxValue))
+        dispatch(setCurrentAC(minValue))
+    }
 
     const minCallBack = (e: ChangeEvent<HTMLInputElement>) => {
         settingMINimalValueCallBack(Number(e.currentTarget.value))
